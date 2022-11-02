c3da0d6be527fe7ae3771398859c459c4c1f9922
DeVonte G <dagray92@hotmail.com>
2022-11-01 23:18:38 -0400

    minor card css

--- pages/urls.py
+++ pages/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 from .views import (
     HomePageView,
     AboutPageView,
-    DownloadPageView
+    DownloadPageView,
+    Dev1,
+    Dev2,
+    Dev3,
 )
 
 urlpatterns =[
 
     path('', HomePageView.as_view(), name= 'home'),
+    path('dev1/', Dev1.as_view(), name= 'dev1'),
+    path('dev2/', Dev2.as_view(), name= 'dev2'),
+    path('dev3/', Dev3.as_view(), name= 'dev3'),
     path('about/', AboutPageView.as_view(), name= 'about'),
     path('download/', DownloadPageView.as_view(), name= 'download'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -10,3 +10,12 @@ class AboutPageView(TemplateView):
 
 class DownloadPageView(TemplateView):
     template_name = 'pages/download.html'
+
+class Dev1(TemplateView):
+    template_name=  'pages/dev1.html'
+
+class Dev2(TemplateView):
+    template_name= 'pages/dev2.html'
+
+class Dev3(TemplateView):
+    template_name= 'pages/dev3.html'
